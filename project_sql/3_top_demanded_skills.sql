4d070cf9794db9dc4b660c50c99fe5f212e1a83c
Allen Isaac <allenisaac04062001@gmail.com>
2025-07-17 00:22:41 +0530

    3_top_demanded_skills

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,6 +1,15 @@
-select 
-    count(skill_id) as skill_count,
-    skills,
-    type as skill_type   
-FROM
-    skills_dim
+Select 
+count(skills_job_dim.job_id) as total_jobs,
+skills
+
+from job_postings_fact
+inner join skills_job_dim
+    on job_postings_fact.job_id = skills_job_dim.job_id
+inner join skills_dim
+    on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title='Data Analyst' 
+Group BY
+    skills
+order by total_jobs DESC
+limit 10;
